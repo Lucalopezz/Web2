72efc4ffc5cb1f7b5325f8e2890b0e26741d31bb
Lucas Lopes <lucaslops12@gmail.com>
2026-03-16 08:13:53 -0300

    Add teacher project

---
+++ prw2_profsifsp/src/App.jsx
@@ -0,0 +1,82 @@
+
+import React, { useState } from "react";
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Formulario/Formulario";
+import Area from "./componentes/Area/Area";
+
+function App() {
+
+  const areas = [
+
+    {
+      nome : 'Progr. Básica',
+      corPrimaria : '#57c278',
+      corSecundaria : '#d9f7e9'
+    },
+    {
+      nome : 'Progr. Web',
+      corPrimaria : '#82cffa',
+      corSecundaria : '#e8f8ff'
+    },
+    {
+      nome : 'Banco de Dados',
+      corPrimaria : '#a6d157',
+      corSecundaria : '#f0f8e2'
+    },
+    {
+      nome : 'Diversos',
+      corPrimaria : '#e06b69',
+      corSecundaria : '#fde7e8'
+    }
+
+  ];
+
+  // Criando uma lista só com os nomes das áreas: 
+  const listaAreas = areas.map( area => area.nome );
+
+	// lista (array) de professores.
+	// o valor inicial é um array vazio []
+	const [professores, setProfessores] = useState( [] )
+
+  const adicionaProf = (prof) => {
+
+    // criando uma nova lista com o conteúdo anterior da lista.
+    //
+    // CUIDADO!!! não vá fazer listaNova = professores,
+    // isso não criará uma nova lista, apenas criará uma nova
+    // referência (ponteiro) para a mesma lista (o mesmo objeto).
+    //
+    // O método slice() de um array em JavaScript 
+    // retorna uma cópia superficial de uma porção de um array 
+    // dentro de um novo array objeto selecionado desde start 
+    // até end (end não é incluído). 
+    // Se não passarmos nenhum argumento para slice(), 
+    // ele retorna uma cópia do array inteiro.
+    const listaNova = professores.slice();
+
+    // adicionando o novo professor nesssa lista:
+    listaNova.push(prof)
+
+    // colocando a nova lista atualizada no estado:
+    setProfessores( listaNova )
+  }
+
+	return (
+    <div>
+      <Banner />
+      <Formulario areas={listaAreas} aoProfCadastrado={prof => adicionaProf(prof)} />
+
+      { areas.map(area => <Area key={area.nome}
+                                nome={area.nome}
+                                corPrimaria={area.corPrimaria}
+                                corSecundaria={area.corSecundaria}
+                                professores={ professores.filter( prof => prof.area === area.nome ) }
+                          />) 
+      }
+
+    </div>
+  );
+}
+
+export default App;
+
---
+++ prw2_profsifsp/src/componentes/Formulario/Formulario.jsx
@@ -0,0 +1,77 @@
+
+import React, { useState } from "react";
+import './Formulario.css';
+import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
+import Botao from "../Botao/Botao";
+
+const Formulario = (props) => {
+
+    // Criando os estados...
+    const [nome, setNome] = useState('')
+    const [titulo, setTitulo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [area, setArea] = useState('Progr. Básica')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        //console.log('Form foi submetido => ', nome, titulo, imagem, area)
+        props.aoProfCadastrado(
+            {
+                "nome" : nome,
+                "titulo" : titulo,
+                "imagem" : imagem,
+                "area" : area
+            }
+        );
+        setNome('');
+        setTitulo('');
+        setImagem('');
+        setArea('Progr. Básica');
+    }
+
+    return (
+        <section className="formulario">
+
+            <form onSubmit={aoSalvar}>
+
+                <h2>Dados do professor:</h2>
+
+                <CampoTexto
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+
+                <CampoTexto
+                    label="Título"
+                    placeholder="Digite seu título"
+                    valor={titulo}
+                    aoAlterado={valor => setTitulo(valor)}
+                />
+
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa
+                    label="Áreas"
+                    itens={props.areas}
+                    valor={area}
+                    aoAlterado={valor => setArea(valor)}
+                />
+
+                <Botao>Criar Professor</Botao>
+
+            </form>
+
+        </section>
+    )
+}
+
+export default Formulario;
+
